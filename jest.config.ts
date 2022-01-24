143530f92ee19da8486ab62a4f46f146393925c4
paulo0056 <pauloandre.dev@gmail.com>
2022-01-24 13:44:40 -0300

    chore: test jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,11 @@
 export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  colletcCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-test'
+    '.+\\.ts$': 'ts-jest'
   }
 }
