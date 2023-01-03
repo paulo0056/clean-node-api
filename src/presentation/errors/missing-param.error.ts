6c5a9e5eb3909a3bcc918db8c86dae133856751a
paulo0056 <pauloandre.dev@gmail.com>
2023-01-03 20:20:10 -0300

    refactor: add customized error for missing param

---
+++ src/presentation/errors/missing-param.error.ts
@@ -0,0 +1,6 @@
+export class MissingParamError extends Error {
+  constructor(paramName: string) {
+    //classes que herdam de erro no typescript precisa ser super
+    super(`Missing param: ${paramName}`);
+  }
+}
